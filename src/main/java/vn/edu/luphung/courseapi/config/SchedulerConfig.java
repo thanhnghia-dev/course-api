de062fbeafec6bdf946d340eadb444897557853c
hothanhnghia-nlu <nghiant369@gmail.com>
2025-01-16 10:54:55 +0700

    Config run api continuously

---
+++ src/main/java/vn/edu/luphung/courseapi/config/SchedulerConfig.java
@@ -0,0 +1,9 @@
+package vn.edu.luphung.courseapi.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+public class SchedulerConfig {
+}
